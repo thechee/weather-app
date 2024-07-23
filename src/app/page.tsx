a541efa2c21faa4f47c1d00a38e9d77bf35e7f96
Chase Agee <105754333+thechee@users.noreply.github.com>
2024-07-22 20:35:29 -0700

    finished walkthrough

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import { metersToKilometers } from "@/utils/metersToKilometers";
 import { convertWindSpeed } from "@/utils/convertWindSpeed";
 import ForecastWeatherDetail from "@/components/ForecastWeatherDetail";
 import { useAtom } from "jotai";
-import { placeAtom } from "./atom";
+import { loadingCityAtom, placeAtom } from "./atom";
 import { useEffect } from "react";
 
 
@@ -90,6 +90,7 @@ interface Coord {
 
 export default function Home() {
   const [place, setPlace] = useAtom(placeAtom);
+  const [loadingCity, ] = useAtom(loadingCityAtom)
 
   const { isLoading, error, data, refetch } = useQuery<WeatherData>(
     'repoData', 
@@ -133,82 +134,123 @@ export default function Home() {
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <NavBar />
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
-        <section className="space-y-4">
-          <div className="space-y-2">
-            <h2 className="flex gap-1 text-2xl items-end">
-              <p>{format(parseISO(firstData?.dt_txt ?? ''), 'EEEE')}</p>
-              <p className="text-base">{format(parseISO(firstData?.dt_txt ?? ''), 'MM-dd-yyyy')}</p>
-            </h2>
-            <Container className="gap-10 px-6 items-center">
-              <div className="flex flex-col px-4 text-center">
-                <span className="text-5xl">
-                  {convertKelvinToCelcius(firstData?.main.temp ?? 0)}°C
-                </span>
-                <p className="text-xs space-x-1 whitespace-nowrap">
-                  <span>Feels like</span>
-                  <span>{convertKelvinToCelcius(firstData?.main.feels_like ?? 0)}°</span>
-                </p>
-                <p className="flex text-xs space-x-2 justify-between px-4">
-                  <span className="flex">{convertKelvinToCelcius(firstData?.main.temp_min ?? 0)}°<FaTemperatureArrowDown className="self-center"/></span>
-                  <span className="flex">{convertKelvinToCelcius(firstData?.main.temp_max ?? 0)}°<FaTemperatureArrowUp className="self-center"/></span>
-                </p>
-              </div>
-              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
-                {data?.list.map((item, index) => (
-                  <div 
-                    key={index} 
-                    className="flex flex-col items-center justify-between gap-2 text-xs font-semibold"
-                  >
-                    <p className="whitespace-nowrap">{format(parseISO(item.dt_txt), 'h:mm a')}</p>
-                    <WeatherIcon iconName={getDayOrNightIcon(item.weather[0].icon, data.city.sunrise, data.city.sunset, item.dt_txt)} />
-                    <p>{convertKelvinToCelcius(item?.main.temp ?? 0)}°C</p>
+        {loadingCity ? (
+          <WeatherSkeleton />
+        ) : (
+          <>
+            <section className="space-y-4">
+              <div className="space-y-2">
+                <h2 className="flex gap-1 text-2xl items-end">
+                  <p>{format(parseISO(firstData?.dt_txt ?? ''), 'EEEE')}</p>
+                  <p className="text-base">{format(parseISO(firstData?.dt_txt ?? ''), 'MM-dd-yyyy')}</p>
+                </h2>
+                <Container className="gap-10 px-6 items-center">
+                  <div className="flex flex-col px-4 text-center">
+                    <span className="text-5xl">
+                      {convertKelvinToCelcius(firstData?.main.temp ?? 0)}°C
+                    </span>
+                    <p className="text-xs space-x-1 whitespace-nowrap">
+                      <span>Feels like</span>
+                      <span>{convertKelvinToCelcius(firstData?.main.feels_like ?? 0)}°</span>
+                    </p>
+                    <p className="flex text-xs space-x-2 justify-between px-4">
+                      <span className="flex">{convertKelvinToCelcius(firstData?.main.temp_min ?? 0)}°<FaTemperatureArrowDown className="self-center"/></span>
+                      <span className="flex">{convertKelvinToCelcius(firstData?.main.temp_max ?? 0)}°<FaTemperatureArrowUp className="self-center"/></span>
+                    </p>
                   </div>
-                ))}
+                  <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                    {data?.list.map((item, index) => (
+                      <div 
+                        key={index} 
+                        className="flex flex-col items-center justify-between gap-2 text-xs font-semibold"
+                      >
+                        <p className="whitespace-nowrap">{format(parseISO(item.dt_txt), 'h:mm a')}</p>
+                        <WeatherIcon iconName={getDayOrNightIcon(item.weather[0].icon, data.city.sunrise, data.city.sunset, item.dt_txt)} />
+                        <p>{convertKelvinToCelcius(item?.main.temp ?? 0)}°C</p>
+                      </div>
+                    ))}
+                  </div>
+                </Container>
               </div>
-            </Container>
-          </div>
-          <div className="flex gap-4">
-                <Container className="w-fit justify-center flex-col px-4 items-center">
-                  <p className="capitalize text-center">{firstData?.weather[0].description}</p>
-                  <WeatherIcon 
-                    iconName={getDayOrNightIcon(firstData?.weather[0].icon ?? "", data?.city.sunrise ?? 0, data?.city.sunset ?? 0, firstData?.dt_txt ?? "")} 
-                  />
-                </Container>
-                <Container className="bg-yellow-300/80 px-6 gap-4 justify-between overflow-x-auto">
-                  <WeatherDetails 
-                    visibility={metersToKilometers(firstData?.visibility ?? 1000)}
-                    humidity={`${firstData?.main.humidity}%`}
-                    windSpeed={convertWindSpeed(firstData?.wind.speed ?? 0)}
-                    airPressure={`${firstData?.main.pressure}hPa`}
-                    sunrise={format(fromUnixTime(data?.city.sunrise ?? 0), 'h:mm a')}
-                    sunset={format(fromUnixTime(data?.city.sunset ?? 0), 'h:mm a')}
-                  />
-                </Container>
-          </div>
-        </section>
-        <section className="flex w-full flex-col gap-4">
-          <p className="text-2xl">Forecast <span className="text-sm">7 days</span></p>
-          {firstDataForEachDate.map((item, index) => (
-            <ForecastWeatherDetail
-              key={index}
-              description={item?.weather[0].description ?? ""}
-              feels_like={item?.main.feels_like ?? 0}
-              temp={item?.main.temp ?? 0}
-              temp_max={item?.main.temp_max ?? 0}
-              temp_min={item?.main.temp_min ?? 0}
-              weatherIcon={item?.weather[0].icon ?? ""}
-              date={format(parseISO(item?.dt_txt ?? ''), 'MM-dd')}
-              day={format(parseISO(item?.dt_txt ?? ''), 'EEEE')}
-              airPressure={`${item?.main.pressure} hPa`}
-              humidity={`${item?.main.humidity}%`}
-              sunrise={format(fromUnixTime(data?.city.sunrise ?? 0), 'h:mm a')}
-              sunset={format(fromUnixTime(data?.city.sunset ?? 0), 'h:mm a')}
-              visibility={`${metersToKilometers(item?.visibility ?? 1000)}`}
-              windSpeed={convertWindSpeed(item?.wind.speed ?? 0)}
-            />
-          ))}
-        </section>
+              <div className="flex gap-4">
+                    <Container className="w-fit justify-center flex-col px-4 items-center">
+                      <p className="capitalize text-center">{firstData?.weather[0].description}</p>
+                      <WeatherIcon 
+                        iconName={getDayOrNightIcon(firstData?.weather[0].icon ?? "", data?.city.sunrise ?? 0, data?.city.sunset ?? 0, firstData?.dt_txt ?? "")} 
+                      />
+                    </Container>
+                    <Container className="bg-yellow-300/80 px-6 gap-4 justify-between overflow-x-auto">
+                      <WeatherDetails 
+                        visibility={metersToKilometers(firstData?.visibility ?? 1000)}
+                        humidity={`${firstData?.main.humidity}%`}
+                        windSpeed={convertWindSpeed(firstData?.wind.speed ?? 0)}
+                        airPressure={`${firstData?.main.pressure}hPa`}
+                        sunrise={format(fromUnixTime(data?.city.sunrise ?? 0), 'h:mm a')}
+                        sunset={format(fromUnixTime(data?.city.sunset ?? 0), 'h:mm a')}
+                      />
+                    </Container>
+              </div>
+            </section>
+            <section className="flex w-full flex-col gap-4">
+              <p className="text-2xl">Forecast <span className="text-sm">7 days</span></p>
+              {firstDataForEachDate.map((item, index) => (
+                <ForecastWeatherDetail
+                  key={index}
+                  description={item?.weather[0].description ?? ""}
+                  feels_like={item?.main.feels_like ?? 0}
+                  temp={item?.main.temp ?? 0}
+                  temp_max={item?.main.temp_max ?? 0}
+                  temp_min={item?.main.temp_min ?? 0}
+                  weatherIcon={item?.weather[0].icon ?? ""}
+                  date={format(parseISO(item?.dt_txt ?? ''), 'MM-dd')}
+                  day={format(parseISO(item?.dt_txt ?? ''), 'EEEE')}
+                  airPressure={`${item?.main.pressure} hPa`}
+                  humidity={`${item?.main.humidity}%`}
+                  sunrise={format(fromUnixTime(data?.city.sunrise ?? 0), 'h:mm a')}
+                  sunset={format(fromUnixTime(data?.city.sunset ?? 0), 'h:mm a')}
+                  visibility={`${metersToKilometers(item?.visibility ?? 1000)}`}
+                  windSpeed={convertWindSpeed(item?.wind.speed ?? 0)}
+                />
+              ))}
+            </section>
+          </>
+        )}
       </main>
     </div>
   );
 }
+
+function WeatherSkeleton() {
+  return (
+    <section className="space-y-8">
+      <div className="space-y-2 animate-pulse">
+        <div className="flex gap-1 text-2xl items-end">
+          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+        </div>
+
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          {[1, 2, 3, 4].map((_, index) => (
+            <div key={index} className="flex flex-col items-center space-y-2">
+              <div className="h-6 w-16 bg-gray-300 rounded"></div>
+              <div className="h-6 w-6 bg-gray-300 rounded-full"></div>
+              <div className="h-6 w-16 bg-gray-300 rounded"></div>
+            </div>
+          ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 animate-pulse">
+          <p className="text-2xl h-8 w-36 bg-gray-300 rounded"></p>
+          {[1, 2, 3, 4, 5, 6, 7].map((_, index) => (
+            <div key={index} className="grid grid-cols-2 md:grid-cols-4 gaps-4">
+              <div className="h-8 w-28 bg-gray-300 rounded"></div>
+              <div className="h-10 w-10 bg-gray-300 rounded-full"></div>
+              <div className="h-8 w-28 bg-gray-300 rounded"></div>
+              <div className="h-8 w-28 bg-gray-300 rounded"></div>
+            </div>
+          ))}
+        </div>
+    </section>
+  );
+};
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ import { MdWbSunny, MdMyLocation, MdOutlineLocationOn } from "react-icons/md";
 import SearchBox from './SearchBox';
 import axios from 'axios';
 import { useAtom } from 'jotai';
-import { placeAtom } from '@/app/atom';
+import { loadingCityAtom, placeAtom } from '@/app/atom';
 
 type Props = {}
 
@@ -17,6 +17,7 @@ export default function NavBar({}: Props) {
   const [suggestions, setSuggestions] = useState<string[]>([])
   const [showSuggestions, setShowSuggestions] = useState(false)
   const [place, setPlace] = useAtom(placeAtom)
+  const [_, setLoadingCity] = useAtom(loadingCityAtom)
 
   async function handleInputChange(value: string) {
     setCity(value)
@@ -47,46 +48,95 @@ export default function NavBar({}: Props) {
   }
 
   function handleSearchSubmit(e: React.FormEvent<HTMLFormElement>) { 
+    setLoadingCity(true);
     e.preventDefault();
     if (suggestions.length == 0) {
       setError('Location not found');
+      setLoadingCity(false);
     } else {
       setError('');
-      setPlace(city);
-      setCity('');
-      setShowSuggestions(false);
+      setTimeout(() => {
+        setLoadingCity(false);
+        setPlace(city);
+        setCity('');
+        setShowSuggestions(false);
+      }, 1000);
+    }
+  }
+
+  function handleCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const { latitude, longitude } = position.coords;
+
+        try {
+          setLoadingCity(true);
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+          );
+          setTimeout(() => {
+            setLoadingCity(false);
+            setPlace(response.data.name);
+          }, 1000);
+        } catch (error) {
+          setLoadingCity(false);
+        }
+      });
     }
   }
 
   return (
-    <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
-      <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
-        <div className='flex items-center justify-center gap-2'>
-          <h2 className='text-gray-500 text-3xl'>Weather</h2>
-          <MdWbSunny className='text-3xl mt-1 text-yellow-300'/>
-        </div>
-        <section className='flex gap-2 items-center'>
-          <MdMyLocation className='text-2xl text-gray-400 hover:opacity-80 cursor-pointer'/>
-          <MdOutlineLocationOn className='text-3xl'/>
-          <p className='text-slate-900/80 text-sm'>{place}</p>
-          <div className='relative'>
-            <SearchBox 
-              value={city}
-              onSubmit={e => handleSearchSubmit(e)}
-              onChange={e => handleInputChange(e.target.value)}
-            />
-            <SuggestionBox 
-              {...{
-                showSuggestions, 
-                suggestions, 
-                handleSuggestionClick, 
-                error
-              }}
-            />
+    <>
+      <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
+        <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
+          <div className='flex items-center justify-center gap-2'>
+            <h2 className='text-gray-500 text-3xl'>Weather</h2>
+            <MdWbSunny className='text-3xl mt-1 text-yellow-300'/>
           </div>
-        </section>
-      </div>
-    </nav>
+          <section className='flex gap-2 items-center'>
+            <MdMyLocation 
+              className='text-2xl text-gray-400 hover:opacity-80 cursor-pointer'
+              title='Current Location'
+              onClick={handleCurrentLocation}
+            />
+            <MdOutlineLocationOn className='text-3xl'/>
+            <p className='text-slate-900/80 text-sm'>{place}</p>
+            <div className='relative hidden md:flex'>
+              <SearchBox 
+                value={city}
+                onSubmit={e => handleSearchSubmit(e)}
+                onChange={e => handleInputChange(e.target.value)}
+              />
+              <SuggestionBox 
+                {...{
+                  showSuggestions, 
+                  suggestions, 
+                  handleSuggestionClick, 
+                  error
+                }}
+              />
+            </div>
+          </section>
+        </div>
+      </nav>
+      <section className='flex m-w-7xl px-3 md:hidden'>
+        <div className='relative'>
+          <SearchBox 
+            value={city}
+            onSubmit={e => handleSearchSubmit(e)}
+            onChange={e => handleInputChange(e.target.value)}
+          />
+          <SuggestionBox 
+            {...{
+              showSuggestions, 
+              suggestions, 
+              handleSuggestionClick, 
+              error
+            }}
+          />
+        </div>
+      </section>
+    </>
   )
 }
 
